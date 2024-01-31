ebc2603de12fd82d3bed69916f38a7e3b0f8bc5d
Braden Navarro <72952473+bradennavarro@users.noreply.github.com>
2024-01-31 10:21:01 -0700

    Add files via upload

--- test_scripts/groundcamera.py
+++ test_scripts/groundcamera.py
@@ -1,11 +1,26 @@
 """
 Test script takes a few pics with the ground camera
 and downloads them locally to your computer from the drone.
+
+
+** THIS SCRIPT REQUIRES A LOCAL FOLDER OF THE YOLOv5 REPO WITHIN THE 
+   SAME FOLDER AS THIS PYTHON SCRIPT **
+
 """
 
 from pyparrot.Minidrone import Mambo
 import cv2
 import sys
+import os
+import torch
+from pathlib import Path
+from PIL import Image
+
+FILE = Path(__file__).resolve()
+ROOT = FILE.parents[0]  # YOLOv5 root directory
+if str(ROOT) not in sys.path:
+    sys.path.append(str(ROOT))  # add ROOT to PATH
+ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
 
 def save_picture(mambo_object,pictureName,path,filename):
 
@@ -30,6 +45,7 @@ def is_in_the_list(l1,l2):
             return [i,l2[i]]
     return [len(l2)-1,l2[len(l2)-1]]
 
+# Connect to the Mambo Drone
 mambo = Mambo(None, use_wifi=True) #address is None since it only works with WiFi anyway
 print("trying to connect to mambo now")
 success = mambo.connect(num_retries=3)
@@ -38,6 +54,15 @@ print("connected: %s" % success)
 list_of_images = []
 picture_names_new = []
 
+# Custom yolov5 model
+model_path = ROOT / 'best.pt'
+repo_path = ROOT / 'yolov5-master'
+print(ROOT)
+print(model_path)
+print(repo_path)
+model = torch.hub.load(repo_path,'custom', path=model_path, source='local')
+
+# Process for drone to follow while connected
 if (success):
     # get the state information
     mambo.flat_trim()
@@ -104,11 +129,21 @@ if (success):
 
         if frame is not None:
             print("GOT TO HERE")
-            filename = "test_image_%02d.png" % c
-            save_picture(mambo,picture_name,path,filename)
+            image = "test_image_%02d.png" % c
+            save_picture(mambo,picture_name,path,image)
+            image = ROOT / image
+            results = model(image)
+            for det in results.xyxy[0]:
+                x_min, y_min, x_max, y_max, confidence, class_label = det
+                if confidence > 0.70:
+                    print("LANDING PAD SPOTTED!")
+                    results.save()
+                    mambo.safe_land(5)
+                    mambo.disconnect()
+                    break
             c = c+1
 
-        if c>5:
+        if c>20:
             break
 
 
